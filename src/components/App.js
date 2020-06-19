89d2e72072657fe13edf979afa39ef2f4fdd4418
Hiroshi Takaichi <hirosi.t822@gmail.com>
2020-06-19 23:49:43 +0900

    implement all events deletion event

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,13 @@ const App = () => {
     setBody('');
   }
 
+  const deleteAllEvents = e => {
+    e.preventDefault();
+    const result = window.confirm('全てのイベントを本当に削除しても良いですか？');
+    if (result) dispatch({ type: 'DELETE_ALL_EVENTS' });
+  }
 
+  const unCreatable = title === '' || body === '';
 
   return (
     <div className="container-fluid">
@@ -37,8 +43,8 @@ const App = () => {
           <textarea className="form-control" id="formEventBody" value={body} onChange={(e) => setBody(e.target.value)} />
         </div>
 
-        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
       </form>
 
       <h4>イベント一覧</h4>
@@ -52,7 +58,7 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          { state.map((event,idnex) => (<Event key={idnex} event={event} dispatch={dispatch} />))}
+          {state.map((event, idnex) => (<Event key={idnex} event={event} dispatch={dispatch} />))}
         </tbody>
       </table>
     </div>
